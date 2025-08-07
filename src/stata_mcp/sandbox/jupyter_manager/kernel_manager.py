0b130272b4a7edcf0a946e24b5c715dd115e6fb7
SongTan <sepinetam@gmail.com>
2025-08-07 20:39:37 +0800

    add: make sandbox infrastructure
    Co-Authored-By: StataMCP-Team <223989137+statamcp-team@users.noreply.github.com>

---
+++ src/stata_mcp/sandbox/jupyter_manager/kernel_manager.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 - Present Sepine Tam, Inc. All Rights Reserved
+#
+# @Author : Sepine Tam (谭淞)
+# @Email  : sepinetam@gmail.com
+# @File   : kernel_manager.py
+
